cdac4bfa9605cb110f3e6139ea147fd16322384e
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-09 21:48:33 -0300

    chore: teste ci

--- src/main/java/br/com/adega/adegaApp/controllers/Hello.java
+++ src/main/java/br/com/adega/adegaApp/controllers/Hello.java
@@ -13,6 +13,6 @@ public class Hello {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public String hello(){
-        return "Hello, World 6!";
+        return "Hello, World 7!";
     }
 }
