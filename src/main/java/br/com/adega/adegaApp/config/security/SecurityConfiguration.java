92ba44ba9b7bd2e1ab73f58d362f12f7e04863bf
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-09-06 10:41:45 -0300

    feat: liberando acesso a alguns endpoints

--- src/main/java/br/com/adega/adegaApp/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/adega/adegaApp/config/security/SecurityConfiguration.java
@@ -1,10 +1,32 @@
 package br.com.adega.adegaApp.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    // Configurações de Autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    // Configurações de Autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/funcionarios").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/funcionarios/*").permitAll();
+    }
+
+    // Configurações de recursos estáticos
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
 }
